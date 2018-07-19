a0da738446e850d7c554e2b8255e5f7209b9b2d3
Evert De Boeck <git@komkommer.be>
2018-07-19 23:21:05 +0200

    Ch01Tut02-05
    Chapter 01
    Tutorial 02 (05 in explanation - from now on, we change to the numbering from the tutorials)

---
+++ src/main/scala/com/allaboutscala/chapter/one/tutorial_05/HelloWorldMain.scala
@@ -0,0 +1,10 @@
+package com.allaboutscala.chapter.one.tutorial_05
+
+object HelloWorldMain {
+  // Function   : main
+  // Parameters : 1: arg - type = Array of String
+  // Return type: Unit (= void)
+  def main(args: Array[String]): Unit = {
+    println("Hello World from main function!")
+  }
+}
